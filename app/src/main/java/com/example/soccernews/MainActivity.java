2b8a2a913cad9afa26bb0b79188288706bf01a2a
arthu <arthurguilhermesb@gmail.com>
2022-06-13 20:53:39 -0300

    Fix: Problem with background of ImageView in his XML file

--- app/src/main/java/com/example/soccernews/MainActivity.java
+++ app/src/main/java/com/example/soccernews/MainActivity.java
@@ -23,8 +23,8 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_news, R.id.navigation_favorites)
-                .build();
+                R.id.navigation_news, R.id.navigation_favorites).build();
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
