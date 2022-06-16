0634e462b64809f58d54e0cfbe42672d87550ec8
arthu <arthurguilhermesb@gmail.com>
2022-06-16 18:50:28 -0300

    Fix: Run RoomDatabase in Main Thread

--- app/src/main/java/com/example/soccernews/ui/news/NewsFragment.java
+++ app/src/main/java/com/example/soccernews/ui/news/NewsFragment.java
@@ -1,5 +1,6 @@
 package com.example.soccernews.ui.news;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,6 +9,7 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.loader.content.AsyncTaskLoader;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.room.Room;
 
@@ -31,15 +33,15 @@ public class NewsFragment extends Fragment {
         binding = FragmentNewsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        db = Room.databaseBuilder(getContext(), AppDatabase.class, "soccer-news").build();
+        db = Room.databaseBuilder(getContext(), AppDatabase.class, "soccer-news")
+                .allowMainThreadQueries()
+                .build();
 
         binding.rvNews.setLayoutManager(new LinearLayoutManager(getContext()));
         newsViewModel.getNews().observe(getViewLifecycleOwner(), news -> {
             binding.rvNews.setAdapter(new NewsAdapter(news, updatedNews -> {
                 db.newsDao().insert(updatedNews);
-
             }));
-
         });
         return root;
     }
